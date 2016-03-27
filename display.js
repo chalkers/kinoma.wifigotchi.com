a2f6ac8e3bfbcddf1957cf67384bab357d63c5a5
Andrew Chalkley <andrew@chalkley.org>
2016-03-27 16:59:56 -0400

    Initial bitmap writing

---
+++ display.js
@@ -0,0 +1,20 @@
+//@module
+const COMMANDS = {
+	TURN_ON_OSCILLATOR: 0x21,
+	TURN_ON_DISPLAY: 0x81,
+	SET_BRIGHTNESS: 0xE0
+}
+
+exports.pins = {    display: {type: "I2C", address: 0x70}};exports.configure = function () {	this.display.init();}exports.close = function() {
+    this.display.close();}exports.start = function (options) {
+	this.display.writeByte(COMMANDS.TURN_ON_OSCILLATOR);	this.display.writeByte(COMMANDS.TURN_ON_DISPLAY);
+	this.display.writeByte(COMMANDS.SET_BRIGHTNESS | options.brightness);
+}
+
+exports.writeBitmap = function(array) {
+	array.forEach((element, index) => {
+		this.display.writeByteDataSMB(index * 2, rotateRight(element));
+	});
+}
+
+function rotateRight(value) {    //Shift everything right 1 bit    //Then shift last bit over if switched on it'll switch on 2^7    return (value >> 1) | (value << 7);};
---
+++ main.js
@@ -0,0 +1,22 @@
+import Pins from "pins";
+
+const faces = {
+	smile: [    0b00111100,    0b01000010,    0b10100101,    0b10000001,    0b10100101,    0b10011001,    0b01000010,    0b00111100	],
+	neutral : [    0b00111100,    0b01000010,    0b10100101,    0b10000001,    0b10111101,    0b10000001,    0b01000010,    0b00111100	],
+	frown: [    0b00111100,    0b01000010,    0b10100101,    0b10000001,    0b10011001,    0b10100101,    0b01000010,    0b00111100	]	
+}
+
+function randomFace(){
+	const faceTypes = Object.keys(faces);
+	const randomFaceIndex = Math.floor(Math.random() * faceTypes.length);
+	const randomFaceKey = faceTypes[randomFaceIndex];
+	return faces[randomFaceKey];
+}
+
+var main = {	onLaunch(){		Pins.configure({			display: {				require: "display",				pins: {					display: {  sda: 13, clock: 14},					ground: { pin: 12, type: "Ground"},					power: { pin: 11, type: "Power"}				} 			},		}, (success) => {			if (success) {
+				Pins.invoke("/display/start", {brightness: 0});
+				setInterval(()=> {
+					Pins.invoke("/display/writeBitmap", randomFace());
+				}, 750);
+			} else {				trace("Failed to configure\n");			}		});	}};
+export default main;
